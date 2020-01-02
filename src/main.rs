41e36c477cb39c7b20d4134d8bae1ef1db4386bc
Travis Johnson <travis@jumprope.com>
2020-01-02 17:20:33 -0500

    printing tags

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,22 @@ extern crate git2;
 use git2::Repository;
 
 fn main() {
-    let repo = match Repository::open("./") {
+    let repo = match Repository::open(".") {
         Ok(repo) => repo,
         Err(e) => panic!("failed to open: {}", e),
     };
 
-    let tags = match repo.tag_names(Some("www")) {
+    let tags = match repo.tag_names(Some("v*")) {
         Ok(tags) => tags,
         Err(e) => panic!("could not find any tags {}", e),
     };
 
-    println!("found {} tags", tags.len())
+    println!("found {} tags", tags.len());
+    for tag in tags.iter() {
+        let name = match tag {
+            Some(name) => name,
+            None => "unknown",
+        };
+        println!("Tag: {}", name);
+    }
 }
