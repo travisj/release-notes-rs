b18d9f92377e782bb67a9bfd4740aec718de596a
Travis Johnson <travis@jumprope.com>
2020-01-04 15:58:56 -0500

    able to traverse the repo and see commits

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ extern crate regex;
 extern crate semver;
 extern crate structopt;
 
-use git2::Repository;
+use git2::{Commit, Repository};
+
 use regex::Regex;
 use semver::Version;
 use structopt::StructOpt;
@@ -16,6 +17,47 @@ struct Cli {
     #[structopt(help = "Set Semver Increment Type", default_value = "patch")]
     inc_type: String,
 }
+
+fn print_commit(commit: &Commit) {
+    println!("commit {}", commit.id());
+
+    if commit.parents().len() > 1 {
+        print!("Merge:");
+        for id in commit.parent_ids() {
+            print!(" {:.8}", id);
+        }
+        println!();
+    }
+
+    let author = commit.author();
+    println!("Author: {}", author);
+    // print_time(&author.when(), "Date:   ");
+    println!();
+
+    for line in String::from_utf8_lossy(commit.message_bytes()).lines() {
+        println!("    {}", line);
+    }
+    println!();
+}
+
+// fn print_time(time: &Time, prefix: &str) {
+//     let (offset, sign) = match time.offset_minutes() {
+//         n if n < 0 => (-n, '-'),
+//         n => (n, '+'),
+//     };
+//     let (hours, minutes) = (offset / 60, offset % 60);
+//     let ts = time::Timespec::new(time.seconds() + (time.offset_minutes() as i64) * 60, 0);
+//     let time = time::at(ts);
+
+//     println!(
+//         "{}{} {}{:02}{:02}",
+//         prefix,
+//         time.strftime("%a %b %e %T %Y").unwrap(),
+//         sign,
+//         hours,
+//         minutes
+//     );
+// }
 
 fn main() {
     let args = Cli::from_args();
@@ -60,6 +102,28 @@ fn main() {
         "patch" => max_version.increment_patch(),
         _ => max_version.increment_patch(),
     };
+
+    let mut revwalk = repo.revwalk().unwrap();
+    match revwalk.push_head() {
+        Ok(_) => println!("push_head success"),
+        Err(e) => println!("push_head failure: {}", e),
+    };
+    revwalk.set_sorting(git2::Sort::TOPOLOGICAL);
+
+    for commit_id in revwalk.into_iter() {
+        println!("here:");
+        let commit = match commit_id {
+            Ok(id) => {
+                println!("Commit ID: {}", id);
+                match repo.find_commit(id) {
+                    Ok(commit) => commit,
+                    Err(e) => panic!("Error getting commit with id {}: {}", id, e),
+                }
+            }
+            Err(e) => panic!("Error with getting commit in revwalk: {}", e),
+        };
+        print_commit(&commit);
+    }
 
     println!("next version should be: {}", max_version.to_string())
 }
