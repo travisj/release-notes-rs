09d15d7247b081c8e1ea1d4e4d69561006187414
Travis Johnson <travis@jumprope.com>
2020-01-08 22:46:51 -0500

    printing out simple notes for all apps

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate regex;
 extern crate semver;
 extern crate structopt;
 
-use git2::{Commit, Repository};
+use git2::{DiffOptions, Repository};
 
 use regex::Regex;
 use semver::Version;
@@ -18,46 +18,17 @@ struct Cli {
     inc_type: String,
 }
 
-fn print_commit(commit: &Commit) {
-    println!("commit {}", commit.id());
-
-    if commit.parents().len() > 1 {
-        print!("Merge:");
-        for id in commit.parent_ids() {
-            print!(" {:.8}", id);
-        }
-        println!();
-    }
-
-    let author = commit.author();
-    println!("Author: {}", author);
-    // print_time(&author.when(), "Date:   ");
-    println!();
-
-    for line in String::from_utf8_lossy(commit.message_bytes()).lines() {
-        println!("    {}", line);
-    }
-    println!();
+fn get_pathspecs(app: &str) -> Vec<&'static str> {
+    let paths = match app.as_ref() {
+        "admin" => vec!["admin/"],
+        "api" => vec!["server/"],
+        "ios" => vec!["client/ios", "client/app"],
+        "android" => vec!["client/android", "client/app"],
+        "www" => vec!["www/"],
+        _ => vec![],
+    };
+    return paths;
 }
-
-// fn print_time(time: &Time, prefix: &str) {
-//     let (offset, sign) = match time.offset_minutes() {
-//         n if n < 0 => (-n, '-'),
-//         n => (n, '+'),
-//     };
-//     let (hours, minutes) = (offset / 60, offset % 60);
-//     let ts = time::Timespec::new(time.seconds() + (time.offset_minutes() as i64) * 60, 0);
-//     let time = time::at(ts);
-
-//     println!(
-//         "{}{} {}{:02}{:02}",
-//         prefix,
-//         time.strftime("%a %b %e %T %Y").unwrap(),
-//         sign,
-//         hours,
-//         minutes
-//     );
-// }
 
 fn main() {
     let args = Cli::from_args();
@@ -72,8 +43,6 @@ fn main() {
         Ok(tags) => tags,
         Err(e) => panic!("could not find any tags {}", e),
     };
-
-    println!("found {} tags", tags.len());
 
     let mut max_version = Version::parse("0.0.0").unwrap();
 
@@ -96,6 +65,8 @@ fn main() {
         }
     }
 
+    let most_recent_version_string = format!("refs/tags/{}/{}", args.app, max_version.to_string());
+
     match args.inc_type.as_ref() {
         "major" => max_version.increment_major(),
         "minor" => max_version.increment_minor(),
@@ -104,25 +75,56 @@ fn main() {
     };
 
     let mut revwalk = repo.revwalk().unwrap();
-    match revwalk.push_head() {
-        Ok(_) => println!("push_head success"),
-        Err(e) => println!("push_head failure: {}", e),
-    };
+    revwalk.push_head().unwrap();
     revwalk.set_sorting(git2::Sort::TOPOLOGICAL);
 
+    let references = repo.references().unwrap();
+    for reference in references {
+        let reference = reference.unwrap();
+        let name = reference.name().unwrap_or("Unknown");
+        if name == most_recent_version_string {
+            let target = match reference.target() {
+                Some(target) => target,
+                None => panic!("Error getting target"),
+            };
+
+            let _hide = match revwalk.hide(target) {
+                Ok(hide) => hide,
+                Err(e) => panic!("why are we panicking... {}", e),
+            };
+        }
+    }
+
     for commit_id in revwalk.into_iter() {
-        println!("here:");
         let commit = match commit_id {
-            Ok(id) => {
-                println!("Commit ID: {}", id);
-                match repo.find_commit(id) {
-                    Ok(commit) => commit,
-                    Err(e) => panic!("Error getting commit with id {}: {}", id, e),
-                }
-            }
+            Ok(id) => match repo.find_commit(id) {
+                Ok(commit) => commit,
+                Err(e) => panic!("Error getting commit with id {}: {}", id, e),
+            },
             Err(e) => panic!("Error with getting commit in revwalk: {}", e),
         };
-        print_commit(&commit);
+        for parent in commit.parents() {
+            let mut diffopts = DiffOptions::new();
+            for path in get_pathspecs(args.app.as_ref()).into_iter() {
+                diffopts.pathspec(path);
+            }
+            let diff = repo
+                .diff_tree_to_tree(
+                    Some(&parent.tree().unwrap()),
+                    Some(&commit.tree().unwrap()),
+                    Some(&mut diffopts),
+                )
+                .unwrap();
+            if diff.stats().unwrap().files_changed() > 0 {
+                println!(
+                    "* {}",
+                    String::from_utf8_lossy(commit.message_bytes())
+                        .lines()
+                        .next()
+                        .unwrap()
+                );
+            }
+        }
     }
 
     println!("next version should be: {}", max_version.to_string())
